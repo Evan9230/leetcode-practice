860e9fa5d37bc5d8b8a61bc17a8ff008eb6d26ec
dong92302023@163.com <GitHub>
2026-01-13 23:33:05 +0800

    文件夹结构调整

---
+++ .lcpr_data/C/21.合并两个有序链表.c
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=c
+ * @lcpr version=30306
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
